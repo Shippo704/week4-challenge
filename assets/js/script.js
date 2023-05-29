415e49637b3be05ac50022443e02696ecaabd2c6
Shippo704 <matt.taylor704@yahoo.ca>
2023-05-29 10:05:34 -0400

    added start button and associated functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 var timerEl = document.getElementById("timer");
+var startButton = document.getElementById("start");
+var qNum = 0;
+
+var body = document.body;
+var questionEl = document.createElement("p");
+var answerListEl = document.createElement("ul");
+var ans1 = document.createElement("li");
+var ans2 = document.createElement("li");
+var ans3 = document.createElement("li");
+var ans4 = document.createElement("li");
+
+questionEl.textContent ="Question:";
+ans1.textContent = "answer 1";
+ans2.textContent = "answer 2";
+ans3.textContent = "answer 3";
+ans4.textContent = "answer 4";
+
+answerListEl.appendChild(ans1);
+answerListEl.appendChild(ans2);
+answerListEl.appendChild(ans3);
+answerListEl.appendChild(ans4);
 
 function timer() {
     var timeLeft = 90;
@@ -13,5 +34,19 @@ function timer() {
         }
     }, 1000)
 }
+
+startButton.addEventListener("click", function() {
+
+    var quizArea = document.getElementById("quizArea");
+    quizArea.append(questionEl);
+    quizArea.appendChild(answerListEl);
+
+    startButton.style.visibility = "hidden";
+})
+
+function question1() {
+    
+}
+
 
 timer();
